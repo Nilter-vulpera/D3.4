c41266848f0c45b35def1ff1a1d57cd6f991db1c
Danil <74094565+Nilter-vulpera@users.noreply.github.com>
2021-10-07 17:05:18 +0300

    Update urls.py

--- prj/newapp/urls.py
+++ prj/newapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^user_list/', user_list),
     path('product_list/', product_list),
     path('comment_list/', comment_list),
-    path('default/', Default),
+    path('news/', Default),
     path('product/<int:pk>/', ProductDetail.as_view(), name='product_detail'),
     path('delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
     path('create/', ProductCreateView.as_view(), name='product_create'),  # Ссылка на создание товара
